ef9075ba2e1652aed57c0e53d2b302082ce0d8dd
James Smith <kaleburdev@gmail.com>
2024-10-03 20:27:07 -0400

    Add project files.

---
+++ OrangeTests/DatePickerTests.cs
@@ -0,0 +1,46 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace OrangeTests
+{
+    public class Tests
+    {
+        private IWebDriver _driver;
+        private DatePickerPage _datePickerPage;
+        private WebDriverWait _wait;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _datePickerPage = new DatePickerPage(_driver);
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        //[TestCase("8/15/2082")]
+        //[TestCase("5/23/2003")]
+        //[TestCase("10/27/2016")]
+        [TestCase("3/15/1492")]
+        public void SelectDate(string date)
+        {
+            _driver.Navigate().GoToUrl(_datePickerPage.Url);
+            _wait.Until(d => (IJavaScriptExecutor) d).ExecuteScript("return document.readyState").Equals("complete");
+            Assert.That(_datePickerPage.Calendar.Displayed, Is.True);
+            Assert.That(_datePickerPage.CalendarNextButton.Displayed, Is.True);
+            Assert.That(_datePickerPage.CalendarPreviousButton.Displayed, Is.True);
+
+            _datePickerPage.SelectDate(date);
+
+            //Assert.That(_datePickerPage.TargetDateIsSelected(date), Is.True);
+        }
+
+        [TearDown] 
+        public void Teardown() {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
---
+++ OrangeTests/IWebElementExtensions.cs
@@ -0,0 +1,26 @@
+using OpenQA.Selenium;
+using System.Reflection;
+
+namespace OrangeTests
+{
+    public static class IWebElementExtensions
+    {
+        private static IWebDriver? _cachedDriver = null;
+
+        public static void ClickViaJavaScript(this IWebElement element)
+        {
+            if (_cachedDriver is null)
+            {
+                var elementDriver = element.GetType().GetProperty("WrappedDriver")?.GetValue(element);
+                if (elementDriver is null)
+                {
+                    throw new Exception($"{nameof(element)} does not have a web driver.");
+                }
+                _cachedDriver = elementDriver as IWebDriver;
+            }
+
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)_cachedDriver!;
+            javaScriptExecutor.ExecuteScript("arguments[0].click();", element);
+        }
+    }
+}
